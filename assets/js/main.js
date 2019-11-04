120669af0d331512a637d52e28ea3fda5fc9d2bc
damien <d.macors@gmail.com>
2019-11-04 10:32:26 +0100

    honeypot

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let elems = document.querySelectorAll('select');
     let instances = M.FormSelect.init(elems);
 });
+document.getElementById("form").addEventListener("submit",()=>{
+    if (document.getElementById("website").valueOf != null){
+        return false;
+    }
+});
--- getaway.php
+++ getaway.php
@@ -1,4 +1,25 @@
 <?php
+function filterInput(string $value){
+    return  filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS,FILTER_FLAG_NO_ENCODE_QUOTES);
+}
+function checkEmail(string $value){
+    return filter_var($value, FILTER_SANITIZE_EMAIL,FILTER_VALIDATE_EMAIL);
+}
+function checkText(string $value){
+    return filter_var($value,FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+}
+
 foreach ($_POST as $key => $value){
-    echo $key .'='. $value;
+    switch ($key){
+        case "email": checkemail($value);
+            break;
+        case "LastName":
+        case "FirstName":
+        case "message": checkText($value);
+            break;
+        case "genre":
+        case "Subject":
+        case "country": filterInput($value);
+            break;
+    }
 }
